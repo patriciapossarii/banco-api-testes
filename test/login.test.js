063ab4b32f3b92e04b1a41676129ad3c1203180e
patriciapossarii <patricia.mag.carvalho@gmail.com>
2025-09-29 20:28:13 -0300

    Iniciando o projeto

---
+++ test/login.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Login', () => {
+    describe('POST/Login', () => {
+        it('Deve retornar 200 com um token em string quando usar credenciais validas', async () => {
+            const resposta = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456'
+                })
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body.token).to.be.a('string');
+
+        })
+    })
+
+
+})
