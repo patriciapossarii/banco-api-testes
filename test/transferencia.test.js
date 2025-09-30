c0ea32accbf239dd5e6c0221da202b334d47f32c
patriciapossarii <patricia.mag.carvalho@gmail.com>
2025-09-30 07:56:25 -0300

    Criei um helper de autenticação para obter token

---
+++ test/transferencia.test.js
@@ -0,0 +1,48 @@
+const request = require('supertest');
+const { expect } = require('chai');
+require('dotenv').config();
+const { obterToken } = require('../helpers/autenticacao')
+
+describe('Transferencias', () => {
+    describe('POST/Transferencias', () => {
+        it('Deve retornar sucesso 201 quado valor de transferência for igual ou acima de R$ 10,00', async () => {
+            const token = await obterToken('julio.lima', '123456')
+
+            const resposta = await request(process.env.BASE_URL)
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    contaOrigem: 1,
+                    contaDestino: 3,
+                    valor: 11,
+                    token: "string"
+                })
+
+            expect(resposta.status).to.equal(201);
+            expect(resposta.body.message).to.equal('Transferência realizada com sucesso.');
+        })
+
+        it('Deve retornar falha 422 quando valor de transferência for abaixo de R$ 10,00', async () => {
+            const token = await obterToken('julio.lima', '123456')
+
+            const resposta = await request('http://localhost:3000')
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    contaOrigem: 1,
+                    contaDestino: 3,
+                    valor: 9,
+                    token: "string"
+                })
+
+            expect(resposta.status).to.equal(422);
+            expect(resposta.body.error).to.equal('O valor da transferência deve ser maior ou igual a R$10,00.');
+        })
+
+
+    })
+
+})
+
